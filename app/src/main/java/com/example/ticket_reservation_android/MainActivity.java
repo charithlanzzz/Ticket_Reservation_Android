c3d650f3ff7323ffa74650a02a4eec867163e735
charithlanzzz <lansakaracharith@gmail.com>
2023-10-09 02:51:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ticket_reservation_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ticket_reservation_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login);
+    }
+}
